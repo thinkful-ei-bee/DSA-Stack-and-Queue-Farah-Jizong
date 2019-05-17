d0b1acce6215d2f947ce8c0a2665cf1d5e2349a9
Farah Student <farah.gus@hotmail.com>
2019-05-17 11:18:05 -0400

    push and pop methods

--- stack.js
+++ stack.js
@@ -11,6 +11,22 @@ class _Node{
 
 class Stack {
     constructor() {
-        this.top = null;
+        this.top = null; // empty 1st node, indicates top of stack
+    }
+
+    push(data) {
+        if (this.top === null) {
+            this.top = new _Node(data, null);
+            return this.top;
+        }
+
+        const node = new _Node(data, this.top);
+        this.top = node;
+    }
+
+    pop() {
+        const node = this.top;
+        this.top = node.next;
+        return node.data;
     }
 }
